b58c97b6af588a8912a9fec136cfcbab7aca8f53
Jansan <jansan-26@hotmail.com>
2019-06-08 20:28:10 +0200

    Login model created

---
+++ CrmSalesProject/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrmSalesProject.Models
+{
+    public class Login
+    {
+        [Display(Name ="User Name")]
+        [Required(ErrorMessage ="User id email is required")]
+        public string CrmUserId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Display(Name ="CRM Web service")]
+        public string CrmWebServiceUrl { get; set; }
+    }
+}
